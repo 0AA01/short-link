db5877d4d28615db76bb22af18f9a944c7805ca0
AA <1435220156@qq.com>
2024-07-08 11:35:03 +0800

    feature: 后管系统启动程序

---
+++ admin/src/main/java/com/aa03/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,13 @@
+package com.aa03.shortlink.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.aa03.shortlink.admin.dao.mapper")
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
