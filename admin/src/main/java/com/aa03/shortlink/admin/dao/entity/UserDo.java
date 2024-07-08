53ea00cf94e623273e22b1c4b4e2157994dc482e
AA <1435220156@qq.com>
2024-07-08 11:37:28 +0800

    feature: 用户实体类，mapper层

---
+++ admin/src/main/java/com/aa03/shortlink/admin/dao/entity/UserDo.java
@@ -0,0 +1,62 @@
+package com.aa03.shortlink.admin.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户持久层实体
+ */
+@TableName("t_user")
+@Data
+public class UserDo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 注销时间戳
+     */
+    private Long deletionTime;
+
+    /**
+     * 删除标记 0:未删除 1:已删除
+     */
+    private int delFlag;
+}
---
+++ admin/src/main/java/com/aa03/shortlink/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.aa03.shortlink.admin.dao.mapper;
+
+import com.aa03.shortlink.admin.dao.entity.UserDo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户持久层
+ */
+public interface UserMapper extends BaseMapper<UserDo> {
+}
