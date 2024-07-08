021fcde5214a272aadff0a38f33c9b386a736151
AA <1435220156@qq.com>
2024-07-08 12:30:55 +0800

    feature: 实现根据用户名查询无脱敏用户信息

--- admin/src/main/java/com/aa03/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/aa03/shortlink/admin/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.aa03.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.aa03.shortlink.admin.common.convention.result.Result;
 import com.aa03.shortlink.admin.common.convention.result.Results;
 import com.aa03.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.aa03.shortlink.admin.dto.resp.UserActualRespDto;
 import com.aa03.shortlink.admin.dto.resp.UserRespDto;
 import com.aa03.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 用户管理控制层
  */
 @RestController
-@RequestMapping("/api/short-link/v1/user")
+@RequestMapping("/api/short-link/v1/")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -24,9 +27,18 @@ public class UserController {
     /**
      * 根据用户名查询用户信息
      */
-    @GetMapping("/{username}")
+    @GetMapping("/user/{username}")
     public Result<UserRespDto> getUserByUsername(@PathVariable("username") String username) {
         UserRespDto userByUsername = userService.getUserByUsername(username);
+        return Results.success(userByUsername);
+    }
+
+    /**
+     * 根据用户名查询无脱敏用户信息
+     */
+    @GetMapping("/actual/user/{username}")
+    public Result<UserActualRespDto> getUserActualByUsername(@PathVariable("username") String username) {
+        UserActualRespDto userByUsername = BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDto.class);
         return Results.success(userByUsername);
     }
 }
