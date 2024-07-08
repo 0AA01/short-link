c2b2c9ebd8239f96cddb0f08d7b04e36c5434ede
AA <1435220156@qq.com>
2024-07-08 16:38:13 +0800

    feature: 短链接后管Redis缓存常量类

---
+++ admin/src/main/java/com/aa03/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.aa03.shortlink.admin.common.constant;
+
+/**
+ * 短链接后管 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "shor-link:lock_user:register:";
+}
