4a6aa2b9dfb18d1e7e9611672dd539d33d17a70e
AA <1435220156@qq.com>
2024-07-08 16:20:15 +0800

    feature: 布隆过滤器配置

---
+++ admin/src/main/java/com/aa03/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -0,0 +1,26 @@
+package com.aa03.shortlink.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.0001);
+        return cachePenetrationBloomFilter;
+    }
+}
