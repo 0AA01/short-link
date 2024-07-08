35b53cb8f8a1fb276b3f9b7eecf3f2f866e1303e
AA <1435220156@qq.com>
2024-07-08 12:30:23 +0800

    feature: 定义无脱敏响应用户信息

---
+++ admin/src/main/java/com/aa03/shortlink/admin/dto/resp/UserActualRespDto.java
@@ -0,0 +1,26 @@
+package com.aa03.shortlink.admin.dto.resp;
+
+import lombok.Data;
+
+@Data
+public class UserActualRespDto {
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+}
