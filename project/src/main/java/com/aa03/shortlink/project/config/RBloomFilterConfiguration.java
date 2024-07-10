26574d1efe94512c7207dd754a90941be8fdff26
AA <1435220156@qq.com>
2024-07-10 18:48:12 +0800

    feature: 短链接中台项目初始化

---
+++ project/src/main/java/com/aa03/shortlink/project/config/RBloomFilterConfiguration.java
@@ -0,0 +1,23 @@
+package com.aa03.shortlink.project.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止短链接创建注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.0001);
+        return cachePenetrationBloomFilter;
+    }
+}
