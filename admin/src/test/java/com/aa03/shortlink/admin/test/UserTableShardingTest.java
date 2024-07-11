3ba237c8e02eb80eaf507ec3117768dd255c0f04
AA <1435220156@qq.com>
2024-07-11 13:20:39 +0800

    feature: 短链接分组表进行分表

--- admin/src/test/java/com/aa03/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/aa03/shortlink/admin/test/UserTableShardingTest.java
@@ -4,21 +4,19 @@ import org.springframework.util.StringUtils;
 
 public class UserTableShardingTest {
 
-    public static final String SQL = "create table t_user_%s\n" +
+    public static final String SQL = "create table t_group_%s\n" +
             "(\n" +
-            "    id            bigint auto_increment comment 'ID'\n" +
+            "    id          bigint auto_increment comment 'ID'\n" +
             "        primary key,\n" +
-            "    username      varchar(256) null comment '用户名',\n" +
-            "    password      varchar(512) null comment '密码',\n" +
-            "    real_name     varchar(128) null comment '真实姓名',\n" +
-            "    phone         varchar(128) null comment '手机号',\n" +
-            "    mail          varchar(512) null comment '邮箱',\n" +
-            "    create_time   datetime     null comment '创建时间',\n" +
-            "    update_time   datetime     null comment '修改时间',\n" +
-            "    deletion_time bigint       null comment '注销时间戳',\n" +
-            "    del_flag      tinyint(1)   null comment '删除标记 0:未删除 1:已删除',\n" +
-            "    constraint idx_unique_username\n" +
-            "        unique (username)\n" +
+            "    gid         varchar(32)  null comment '分组标识',\n" +
+            "    name        varchar(64)  null comment '分组名称',\n" +
+            "    username    varchar(256) null comment '创建用户',\n" +
+            "    sort_order  int          null comment '分组排序',\n" +
+            "    create_time datetime     null comment '创建时间',\n" +
+            "    update_time datetime     null comment '更新时间',\n" +
+            "    del_flag    tinyint(1)   null comment '删除标识 0：表示未删除 1：表示已删除',\n" +
+            "    constraint idx_unique_username_gid\n" +
+            "        unique (gid, username)\n" +
             ");\n" +
             "\n";
 
