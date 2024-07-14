a7d10d04bab8d90bd6544d0afa6a26ffbd494af8
AA <1435220156@qq.com>
2024-07-14 19:49:13 +0800

    feature: 开发短链接今日监控分表规则

--- project/src/test/java/com/aa03/shortlink/project/test/GenerateTable.java
+++ project/src/test/java/com/aa03/shortlink/project/test/GenerateTable.java
@@ -2,20 +2,23 @@ package com.aa03.shortlink.project.test;
 
 public class GenerateTable {
 
-    public static final String SQL = "CREATE TABLE `t_link_stats_today_%s` (\n" +
-            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识',\n" +
-            "  `full_short_url` varchar(128) DEFAULT NULL COMMENT '短链接',\n" +
-            "  `date` date DEFAULT NULL COMMENT '日期',\n" +
-            "  `today_pv` int(11) DEFAULT '0' COMMENT '今日PV',\n" +
-            "  `today_uv` int(11) DEFAULT '0' COMMENT '今日UV',\n" +
-            "  `today_uip` int(11) DEFAULT '0' COMMENT '今日IP数',\n" +
-            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
-            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
-            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_full-short-url` (`full_short_url`) USING BTREE\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;;";
+    public static final String SQL = "create table t_link_stats_today_%s\n" +
+            "(\n" +
+            "    id             bigint auto_increment comment 'ID'\n" +
+            "        primary key,\n" +
+            "    gid            varchar(32) default 'default' null comment '分组标识',\n" +
+            "    full_short_url varchar(128)                  null comment '短链接',\n" +
+            "    date           date                          null comment '日期',\n" +
+            "    today_pv       int         default 0         null comment '今日PV',\n" +
+            "    today_uv       int         default 0         null comment '今日UV',\n" +
+            "    today_uip      int         default 0         null comment '今日IP数',\n" +
+            "    create_time    datetime                      null comment '创建时间',\n" +
+            "    update_time    datetime                      null comment '修改时间',\n" +
+            "    del_flag       tinyint(1)                    null comment '删除标识 0：未删除 1：已删除',\n" +
+            "    constraint idx_unique_today_stats\n" +
+            "        unique (full_short_url, gid, date)\n" +
+            ");\n" +
+            "\n";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
